413d4bbcd335326b788501b762c33a9b92a786be
victory wagor <wagorvictory@gmail.com>
2026-01-16 12:39:08 +0100

    worked on dashboard

--- lib/config.dart
+++ lib/config.dart
@@ -1,3 +1,4 @@
 final url = 'http://192.168.1.104:3000';
 final registration = '$url/registration';
 final login = '$url/login';
+final addtodo = '${url}storeTodo';
--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:jwt_decoder/jwt_decoder.dart';
+import 'package:reminder_app/config.dart';
+import 'package:velocity_x/velocity_x.dart';
+import 'package:http/http.dart' as http;
 
 class Dashboard extends StatefulWidget {
   final String token;
@@ -10,17 +15,52 @@ class Dashboard extends StatefulWidget {
 }
 
 class _DashboardState extends State<Dashboard> {
-  late String email;
+  late String userId;
+
+  final TextEditingController _todoTitle = TextEditingController();
+  final TextEditingController _todoDesc = TextEditingController();
 
   @override
   void initState() {
     super.initState();
 
-    if (widget.token.isNotEmpty) {
-      Map<String, dynamic> decodedToken = JwtDecoder.decode(widget.token);
-      email = decodedToken['email'] ?? "No Email";
-    } else {
-      email = "No Token Found";
+    // if (widget.token.isNotEmpty) {
+    //   Map<String, dynamic> decodedToken = JwtDecoder.decode(widget.token);
+    //   email = decodedToken['email'] ?? "No Email";
+    // } else {
+    //   email = "No Token Found";
+    // }
+
+    Map<String, dynamic> jwtDecodedToken = JwtDecoder.decode(widget.token);
+
+    userId = jwtDecodedToken['_id'];
+  }
+
+  void addTodo() async {
+    if (_todoTitle.text.isNotEmpty && _todoDesc.text.isNotEmpty) {
+      var regBody = {
+        "userId": userId,
+        "title": _todoTitle.text,
+        "desc": _todoDesc.text,
+      };
+
+      var response = await http.post(
+        Uri.parse(addtodo),
+        headers: {"Content-Type": "application/json"},
+        body: jsonEncode(regBody),
+      );
+
+      var jsonResponse = jsonDecode(response.body);
+
+      print(jsonResponse['status']);
+
+      if (jsonResponse['status']) {
+        _todoDesc.clear();
+        _todoTitle.clear();
+        Navigator.pop(context);
+      } else {
+        print("Something went wrong");
+      }
     }
   }
 
@@ -30,9 +70,60 @@ class _DashboardState extends State<Dashboard> {
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: [Text(email)],
+          children: [Text(userId)],
         ),
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayTextInputDialog(context),
+        tooltip: 'Add-Todo',
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+
+  Future<void> _displayTextInputDialog(BuildContext context) async {
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Add To-do'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _todoTitle,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Title",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+              ).p4().px8(),
+              TextField(
+                controller: _todoDesc,
+                keyboardType: TextInputType.text,
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  hintText: "Description",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                  ),
+                ),
+              ).p4().px8(),
+              ElevatedButton(
+                onPressed: () {
+                  addTodo();
+                },
+                child: Text("Add"),
+              ),
+            ],
+          ),
+        );
+      },
     );
   }
 }
